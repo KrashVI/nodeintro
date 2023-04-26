b3c668f371435b782e9e402e90cb0822e7902fb2
SPRING <ss0848041@dt038559.otc.edu>
2023-04-26 12:53:26 -0500

    added axios

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 import chalk from 'chalk';
+import axios from 'axios';
 
 console.log(chalk.blue('Hello world!'));
 
 console.log(chalk.rgb(123,45,67).underline('Check me out!'));
+import scripts from './scripts.js';
+import {configs} from './scripts.js';
+scripts();
+configs();
+
+axios.get("https://dog.ceo/api/breeds/image/random")
+.then (res=>{
+    console.log(res.data)
+})
---
+++ scripts.js
@@ -0,0 +1,7 @@
+export default function(){
+    console.log("Default function here");
+}
+
+export function configs(){
+    console.log("Gotta make configs with an s");
+}
